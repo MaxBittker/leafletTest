039ea57234480d0f12effb972014e79fa0edb025
maxbittker <maxbittker@gmail.com>
2015-11-26 14:25:16 -0500

    awesome markers

--- paris.js
+++ paris.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([48.8567, 2.3508], 12);
+var map = L.map('map').setView([48.862, 2.3508], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -9,9 +9,108 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
 
 
+var BlueMarker = L.AwesomeMarkers.icon({
+    icon: 'dot-circle-o',
+    prefix: 'fa',
+    markerColor: 'blue'
+});
 
-var marker = L.marker([48.85, 2.33],{opacity: 0.5}).addTo(map);
-var marker = L.marker([48.86, 2.35],{opacity: 0.5}).addTo(map);
-var marker = L.marker([48.87, 2.32],{opacity: 0.5}).addTo(map);
-var marker = L.marker([48.863, 2.37],{opacity: 0.5}).addTo(map);
-var marker = L.marker([48.85, 2.36],{opacity: 1.0}).addTo(map);
+// Lilly - Hostel in Marseille/train from Paris to Marseille
+
+// Shane - Near the eiffel tower in his apartment/Sciences Po paris
+var marker = L.marker([48.8540763, 2.3283652], {
+    opacity: 1.0,
+    icon: BlueMarker
+
+}).addTo(map);
+// Charly - Queen’s campus 
+
+// Katarina - train from Paris to London 
+
+// Candice - dorm room at Aix-Marseille Graduate School of Management
+// var marker = L.marker([43.5863647, 5.4226695], {
+//     opacity: 1.0,
+//     icon: BlueMarker
+// }).addTo(map);
+
+// Claire - restaurant in Paris (10 minutes from the restaurant that got shot) 
+
+// Johnathan - Le Gaulois pub in the 6th arrondissement of Paris
+var marker = L.marker([48.8518864, 2.3560525], {
+    opacity: 1.0,
+    icon: BlueMarker
+}).addTo(map);
+
+
+var Attacks = {
+    "keys": ["1", "2", "5", "3", "4"],
+    "data": {
+        "1": {
+            "cartodb_id": 1,
+            "critical": 0,
+            "dead": 4,
+            "injured": 11,
+            "lats": 48.92445,
+            "longs": 2.36016,
+            "name": "Stade de France",
+            "type_of_attack": "Bombing"
+        },
+        "2": {
+            "cartodb_id": 2,
+            "critical": 10,
+            "dead": 14,
+            "injured": 20,
+            "lats": 48.8717,
+            "longs": 2.36822,
+            "name": "Le Petit Cambodge",
+            "type_of_attack": "Shooting"
+        },
+        "3": {
+            "cartodb_id": 3,
+            "critical": 7,
+            "dead": 80,
+            "injured": 11,
+            "lats": 48.863,
+            "longs": 2.37061,
+            "name": "Bataclan",
+            "type_of_attack": "Shooting"
+        },
+        "4": {
+            "cartodb_id": 4,
+            "critical": 13,
+            "dead": 19,
+            "injured": 23,
+            "lats": 48.8551,
+            "longs": 2.38464,
+            "name": "Charonne\n",
+            "type_of_attack": "Shooting"
+        },
+        "5": {
+            "cartodb_id": 5,
+            "critical": 11,
+            "dead": 4,
+            "injured": 21,
+            "lats": 48.86484,
+            "longs": 2.37674,
+            "name": "Avenue de la République",
+            "type_of_attack": "Shooting"
+        }
+    }
+};
+
+var redMarker = L.AwesomeMarkers.icon({
+    icon: 'flash',
+    prefix: 'fa',
+    markerColor: 'red'
+});
+
+
+L.marker([51.941196, 4.512291], {
+    icon: redMarker
+}).addTo(map);
+Attacks.keys.forEach(key => {
+    var marker = L.marker([Attacks.data[key].lats, Attacks.data[key].longs], {
+        opacity: 1,
+        icon: redMarker
+    }).addTo(map);
+})
